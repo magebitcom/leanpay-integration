ecad8d6194b85eab485b4700cad5a347e0d35c5e
Rihards Ratke <rihards.ratke@magebit.com>
2022-05-10 22:24:39 +0300

    feature/MB-18179 - Implemented API logic

--- Helper/Data.php
+++ Helper/Data.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 namespace Leanpay\Payment\Helper;
 
 use Exception;
-use Magento\Framework\App\Helper\AbstractHelper;
-use Magento\Framework\App\Helper\Context;
-use Magento\Framework\Encryption\EncryptorInterface;
-use Magento\Framework\Exception\NotFoundException;
+use Magento\Quote\Api\Data\CartInterface;
+use Magento\Sales\Api\Data\OrderInterface;
 use Magento\Sales\Model\Order;
 use Magento\Store\Model\ScopeInterface;
+use Magento\Framework\App\Helper\Context;
+use Magento\Framework\Stdlib\DateTime\DateTime;
+use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\Exception\NotFoundException;
+use Magento\Framework\Encryption\EncryptorInterface;
 
 class Data extends AbstractHelper
 {
@@ -126,8 +129,8 @@ class Data extends AbstractHelper
      */
     protected $vendorProductCodes = [
         self::REGULAR_PRODUCT => 'ad2e37e4-b626-429a-9bce-49480532a947',
-        self::ZERO_APR       => '166ede78-6556-47f5-bcdc-25ab57a7d6a1',
-        self::ZERO_PERCENT    => '6ebfd301-e22c-4382-a2bf-cb1d50d20aa2'
+        self::ZERO_APR => '166ede78-6556-47f5-bcdc-25ab57a7d6a1',
+        self::ZERO_PERCENT => '6ebfd301-e22c-4382-a2bf-cb1d50d20aa2'
     ];
     /**
      * Leanpay responses from api
@@ -140,10 +143,23 @@ class Data extends AbstractHelper
         'CANCELED' => Order::STATE_CANCELED,
         'EXPIRED' => Order::STATE_CANCELED
     ];
+
     /**
      * @var EncryptorInterface
      */
     private $encryptor;
+
+    /**
+     * @var DateTime
+     */
+    private $dateTime;
+
+    /**
+     * @var \Magento\Framework\App\ResourceConnection
+     */
+    private $connection;
+
+    private $catalogCategoryFactory;
 
     /**
      * Data constructor.
@@ -151,9 +167,18 @@ class Data extends AbstractHelper
      * @param Context $context
      * @param EncryptorInterface $encryptor
      */
-    public function __construct(Context $context, EncryptorInterface $encryptor)
+    public function __construct(
+        Context                                   $context,
+        EncryptorInterface                        $encryptor,
+        DateTime                                  $dateTime,
+        \Magento\Framework\App\ResourceConnection $connection,
+        \Magento\Catalog\Model\ResourceModel\Category\CollectionFactory $categoryCollectionFactory
+    )
     {
+        $this->connection = $connection;
+        $this->dateTime = $dateTime;
         $this->encryptor = $encryptor;
+        $this->catalogCategoryFactory = $categoryCollectionFactory;
         parent::__construct($context);
     }
 
@@ -164,7 +189,7 @@ class Data extends AbstractHelper
      */
     public function isActive(): bool
     {
-        return (bool) $this->scopeConfig->getValue(self::LEANPAY_IS_ACTIVE_PATH);
+        return (bool)$this->scopeConfig->getValue(self::LEANPAY_IS_ACTIVE_PATH);
     }
 
     /**
@@ -186,7 +211,7 @@ class Data extends AbstractHelper
      */
     public function getInstructions(): string
     {
-        return (string) $this->scopeConfig->getValue(
+        return (string)$this->scopeConfig->getValue(
             self::LEANPAY_CONFIG_INSTRUCTIONS_PATH,
             ScopeInterface::SCOPE_STORES
         );
@@ -265,7 +290,7 @@ class Data extends AbstractHelper
      */
     public function getLeanpayLanguage(): string
     {
-        return (string) $this->scopeConfig->getValue(self::LEANPAY_CONFIG_LANG_PATH);
+        return (string)$this->scopeConfig->getValue(self::LEANPAY_CONFIG_LANG_PATH);
     }
 
     /**
@@ -287,7 +312,7 @@ class Data extends AbstractHelper
      */
     private function getEnvironmentMode(): string
     {
-        return (string) $this->scopeConfig->getValue(self::LEANPAY_CONFIG_MODE_PATH);
+        return (string)$this->scopeConfig->getValue(self::LEANPAY_CONFIG_MODE_PATH);
     }
 
     /**
@@ -297,7 +322,7 @@ class Data extends AbstractHelper
      */
     public function getMagentoCheckoutUrl(): string
     {
-        return (string) $this->scopeConfig->getValue(self::LEANPAY_MAGENTO_CHECKOUT_URL);
+        return (string)$this->scopeConfig->getValue(self::LEANPAY_MAGENTO_CHECKOUT_URL);
     }
 
     /**
@@ -366,7 +391,7 @@ class Data extends AbstractHelper
      */
     public function getLeanpayPromosMFPName(): string
     {
-        return (string) $this->scopeConfig->getValue(self::LEANPAY_PROMOS_MFP_PRODUCT_NAME);
+        return (string)$this->scopeConfig->getValue(self::LEANPAY_PROMOS_MFP_PRODUCT_NAME);
     }
 
     /**
@@ -376,7 +401,7 @@ class Data extends AbstractHelper
      */
     public function getLeanpayPromosMFPStartDate(): string
     {
-        return (string) $this->scopeConfig->getValue(self::LEANPAY_PROMOS_MFP_START_DATE);
+        return (string)$this->scopeConfig->getValue(self::LEANPAY_PROMOS_MFP_START_DATE);
     }
 
     /**
@@ -386,7 +411,7 @@ class Data extends AbstractHelper
      */
     public function getLeanpayPromosMFPEndDate(): string
     {
-        return (string) $this->scopeConfig->getValue(self::LEANPAY_PROMOS_MFP_END_DATE);
+        return (string)$this->scopeConfig->getValue(self::LEANPAY_PROMOS_MFP_END_DATE);
     }
 
     /**
@@ -396,7 +421,7 @@ class Data extends AbstractHelper
      */
     public function getLeanpayPromosMFPCartSize(): string
     {
-        return (string) $this->scopeConfig->getValue(self::LEANPAY_PROMOS_MFP_CART_SIZE);
+        return (string)$this->scopeConfig->getValue(self::LEANPAY_PROMOS_MFP_CART_SIZE);
     }
 
     /**
@@ -410,5 +435,158 @@ class Data extends AbstractHelper
         return array_key_exists($name, $this->vendorProductCodes)
             ? $this->vendorProductCodes[$name]
             : '';
+    }
+
+    /**
+     * Need to be able to validate and undestand correct rule
+     *
+     * @return bool
+     */
+    public function getPromoCode($handler): string
+    {
+        if (!$handler instanceof OrderInterface && !$handler instanceof CartInterface) {
+            return '';
+        }
+
+        if ($productMatch = $this->validateProduct($handler)) {
+            return $productMatch;
+        }
+
+        if ($categoryMatch = $this->validateCategory($handler)) {
+            return $categoryMatch;
+        }
+
+        if ($this->validateGlobal($handler)) {
+            return $this->getLeanpayPromosVendorCode();
+        }
+
+        return '';
+    }
+
+    /**
+     * @param $handler
+     * @return false|mixed|void|null
+     */
+    private function validateProduct($handler)
+    {
+        $result = '';
+        if ($handler instanceof OrderInterface || $handler instanceof CartInterface) {
+            $cache = [];
+            foreach ($handler->getItems() as $item) {
+                $product = $item->getProduct();
+                $productValue = $product->getData('leanpay_product_financing_product_value');
+                $vendorCode = $product->getData('leanpay_product_vendor_code');
+                $incluse = $product->getData('leanpay_product_exclusive_inclusive') == 'inclusive' ? 1 : 0;
+                $cache[$item->getProductId()] = $incluse;
+
+                $end = strtotime($product->getData('leanpay_product_end_date') ?? '');
+                $start = strtotime($product->getData('leanpay_product_start_date') ?? '');
+                $isTime = strtotime($product->getData('leanpay_product_time_based') ?? '');
+
+                if ($isTime) {
+                    $currentTime = strtotime($this->dateTime->gmtDate());
+                    if ($start < $currentTime && $currentTime < $end) {
+                        $result = $vendorCode;
+                    }
+                } else {
+                    $result = $vendorCode;
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param $handler
+     * @return string
+     */
+    private function validateCategory($handler)
+    {
+        $result = '';
+        $ids = [];
+        if ($handler instanceof OrderInterface || $handler instanceof CartInterface) {
+            foreach ($handler->getItems() as $item) {
+                $ids[] = $item->getProductId();
+            }
+
+            if (!empty($ids)) {
+                // To improve scaling of the program calculating all required cateogries to compare
+                $ids = implode(',', array_unique($ids));
+                $table = $this->connection->getTableName('catalog_category_product');
+                $string = sprintf('select category_id from %s where product_id in (%s) group by category_id', $table, $ids);
+                $categoriesToCompare = $this->connection->getConnection()->fetchCol($string);
+
+                if (!empty($categoriesToCompare)){
+                   $collection = $this->catalogCategoryFactory->create();
+                    $requiredAttributes = [
+                        'leanpay_category_financing_product_value',
+                        'leanpay_category_priority',
+                        'leanpay_category_time_based',
+                        'leanpay_category_start_date',
+                        'leanpay_category_end_date',
+                        'leanpay_category_vendor_code',
+                        'leanpay_category_exclusive_inclusive'
+                    ];
+                   $categories = $collection->addIdFilter($categoriesToCompare)->addAttributeToSelect($requiredAttributes)->getItems();
+                    if (!empty($categories)) {
+                        foreach ($categories as $category) {
+                            $categoryStart = strtotime($category->getData('leanpay_category_start_date') ?? '');
+                            $categoryEnd = strtotime($category->getData('leanpay_category_end_date') ?? '');
+                            $categoryIsTime = strtotime($category->getData('leanpay_category_time_based') ?? '');
+                            $categoryPriority = (bool)$category->getData('leanpay_category_priority');
+                            $categoryIsExclusive = $category->getData('leanpay_category_exclusive_inclusive') == 'inclusive' ? true : false;
+                            $categoryVendorProduct = $category->getData('leanpay_category_vendor_code');
+                            $categoryProductValue = $category->getData('leanpay_category_financing_product_value');
+                            $currentTime = strtotime($this->dateTime->gmtDate() ?? '');
+
+                            if ($categoryIsTime){
+                                if ($categoryStart < $currentTime && $categoryEnd > $currentTime){
+                                    $result = $categoryVendorProduct;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        return $result;
+    }
+
+    /**
+     * @param $handler
+     * @return bool
+     */
+    private function validateGlobal($handler)
+    {
+        try {
+            $requiredSize = $this->getLeanpayPromosMFPCartSize() ?? 0;
+            $requiredDateStart = strtotime($this->getLeanpayPromosMFPStartDate());
+            $requiredDateEnd = strtotime($this->getLeanpayPromosMFPEndDate());
+            $currentTime = strtotime($this->dateTime->gmtDate());
+
+            // Invalid due to date
+            if ($currentTime < $requiredDateStart && $currentTime > $requiredDateEnd) {
+                return false;
+            }
+
+            $amount = 0.00;
+
+            if ($handler instanceof OrderInterface || $handler instanceof CartInterface) {
+                $amount = (float)$handler->getStore()->getBaseCurrency()->convert(
+                    $handler->getBaseGrandTotal(),
+                    'EUR'
+                );
+            }
+
+            if ($amount < $requiredSize) {
+                return false;
+            }
+
+            return true;
+        } catch (Exception $exception) {
+            return false;
+        }
     }
 }
