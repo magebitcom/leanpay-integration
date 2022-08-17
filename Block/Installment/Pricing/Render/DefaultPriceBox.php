e174824a1d823cc4738a42011f9af7ba08746135
Edgars Patmalnieks <edgars.patmalnieks@magebit.com>
2022-08-17 15:55:37 +0300

    MB-19701 fixing configurable products

--- Block/Installment/Pricing/Render/DefaultPriceBox.php
+++ Block/Installment/Pricing/Render/DefaultPriceBox.php
@@ -13,7 +13,6 @@ use Magento\Framework\Pricing\Render\RendererPool;
 use Magento\Framework\Pricing\SaleableInterface;
 use Magento\Framework\Serialize\SerializerInterface;
 use Magento\Framework\View\Element\Template\Context;
-use Leanpay\Payment\Api\Data\InstallmentInterface;
 
 class DefaultPriceBox extends FinalPriceBox
 {
@@ -131,41 +130,6 @@ class DefaultPriceBox extends FinalPriceBox
     }
 
     /**
-     * Returns JSON config
-     *
-     * @return string
-     */
-    public function getJsonConfig()
-    {
-        $list = $this->installmentHelper->getInstallmentList($this->getAmount());
-        $list = array_values($list);
-        $values = [];
-        $listLength = count($list);
-        for ($index = 0; $index < $listLength; $index++) {
-            $values[] = $index;
-        }
-
-        $data = [
-            'min' => array_key_first($list),
-            'max' => array_key_last($list),
-            'data' => $list,
-            'value' => $values,
-            'currency' => $this->installmentHelper->getCurrencyCode(),
-        ];
-
-        if ($this->installmentHelper->getCurrency() === 'HRK') {
-            $convertedValues = [];
-            foreach ($list as $value) {
-                $convertedValues[] = $this->installmentHelper->getTransitionPrice($value[InstallmentInterface::INSTALLMENT_AMOUNT]);
-            }
-            $data['convertedCurrency'] = 'EUR';
-            $data['convertedValues'] = $convertedValues;
-        }
-
-        return (string) $this->serializer->serialize($data);
-    }
-
-    /**
      * Get Key for caching block content
      *
      * @return string
@@ -173,69 +137,6 @@ class DefaultPriceBox extends FinalPriceBox
     public function getCacheKey()
     {
         return parent::getCacheKey() . ($this->getData('view_key'));
-    }
-
-    public function getCategoryPriceBlock(): string
-    {
-        $price = $this->getLowestInstallmentPrice();
-        if ($this->installmentHelper->getCurrency() === 'HRK') {
-            return $this->_escaper->escapeHtml(__(
-                'od %1 %2 / %3 %4 mjesečno',
-                $price,
-                $this->installmentHelper->getCurrencyCode(),
-                $this->installmentHelper->getTransitionPrice($price),
-                'EUR'
-            ));
-        }
-        return $this->_escaper->escapeHtml(
-            __('ali od %1 %2 / mesec', $price, $this->installmentHelper->getCurrencyCode())
-        );
-    }
-
-    public function getProductPriceBlock(): string
-    {
-        $price = $this->getLowestInstallmentPrice();
-        if ($this->installmentHelper->getCurrency() === 'HRK') {
-           return $this->_escaper->escapeHtml(
-               __('%1 %2 / %3 %4',
-                   $price,
-                   $this->installmentHelper->getCurrencyCode(),
-                   $this->installmentHelper->getTransitionPrice($price),
-                   'EUR'
-               )
-           );
-        }
-        return $this->_escaper->escapeHtml(
-            __('%1 %2', $price, $this->installmentHelper->getCurrencyCode())
-        );
-    }
-
-    public function getTooltipPriceBlock($useTerm = false): string
-    {
-        $data = $this->installmentHelper->getToolTipData($this->getAmount(), $useTerm);
-        $term = $data[InstallmentInterface::INSTALLMENT_PERIOD];
-        $amount = $data[InstallmentInterface::INSTALLMENT_AMOUNT];
-        if ($this->installmentHelper->getCurrency() === 'HRK') {
-            return $this->_escaper->escapeHtml(
-                __('%1 x %2%3 / %4%5',
-                    $term,
-                    $amount,
-                    $this->installmentHelper->getCurrencyCode(),
-                    $this->installmentHelper->getTransitionPrice($amount),
-                    'EUR'
-                )
-            );
-        }
-        return $this->_escaper->escapeHtml(__('%1 x %2%3', $term, $amount, $this->installmentHelper->getCurrencyCode()));
-    }
-
-    public function shouldRenderTooltipPriceBlock($useTerm = false): bool
-    {
-        $data = $this->installmentHelper->getToolTipData($this->getAmount(), $useTerm);
-        return isset(
-            $data[InstallmentInterface::INSTALLMENT_AMOUNT],
-            $data[InstallmentInterface::INSTALLMENT_PERIOD]
-        );
     }
 
 }
