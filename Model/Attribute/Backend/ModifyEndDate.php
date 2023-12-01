b5cc19b1ad50b2805124d4c2a8f2a688bce290f7
Renars Ozols <renars.ozols@magebit.com>
2023-12-01 13:47:49 +0200

    MB-24789 change end-date logic to 23:59:59

---
+++ Model/Attribute/Backend/ModifyEndDate.php
@@ -0,0 +1,39 @@
+<?php
+declare(strict_types=1);
+
+namespace Leanpay\Payment\Model\Attribute\Backend;
+
+use Magento\Eav\Model\Entity\Attribute\Backend\Datetime;
+
+class ModifyEndDate extends Datetime
+{
+    /**
+     * Prepare date for save in DB
+     *
+     * Modify the time part to 23:59:59
+     *
+     * @param string|int|\DateTimeInterface $date
+     * @return string
+     */
+    public function formatDate($date)
+    {
+        // Use parent method to handle date formatting
+        $formattedDate = parent::formatDate($date);
+
+        // Adjust the time portion to 23:59:59
+        return $this->modifyTimeToEndOfDay($formattedDate);
+    }
+
+    /**
+     * Modify time part to 23:59:59
+     *
+     * @param string $formattedDate
+     * @return string
+     */
+    private function modifyTimeToEndOfDay($formattedDate)
+    {
+        $dateTime = new \DateTime($formattedDate);
+        $dateTime->setTime(23, 59, 59);
+        return $dateTime->format('Y-m-d H:i:s');
+    }
+}
